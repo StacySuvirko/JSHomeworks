94e857eb00033d0ea63b9b939e9fd79a1396e9db
Anastasia_Z <fox123_08@mail.ru>
2023-01-12 03:26:10 +0400

    task1 checked

--- js/main.js
+++ js/main.js
@@ -27,3 +27,13 @@ function camelize(str) {
 }
 
 camelize(str);
+
+// correct version
+
+function camelize(str) {
+    return str.split('-')
+    .map((word, index) => index === 0? word: word[0].toUpperCase() + word.slice(1))
+    .join('');
+}
+
+console.log(camelize(str));
