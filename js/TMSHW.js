203772b65e7211c1049d1f7c847ce61bf65f496f
Anastasia_Z <fox123_08@mail.ru>
2023-01-13 03:44:04 +0400

    task2 done

--- js/TMSHW.js
+++ js/TMSHW.js
@@ -1,1 +1,30 @@
 'use strict';
+
+//### 1
+//Написать ф-цию, которая принимает массив объектов типа {name: 'Jonh', age: 20} и возвращает массив значений лежащих в поле age
+
+function getAges(array) {
+    return array.map(obj => obj.age);
+}
+
+console.log(
+    getAges([
+        { name: 'Sergey', age: 33 },
+        { name: 'Daria', age: 33 },
+    ])
+); // => [33, 33]
+
+//### 2
+//Написать ф-цию, которая принимает массив объектов типа {name: 'Jonh', age: 20} и вторым параметром ключ, по которому надо собрать массив
+
+function getValues(array, key) {
+    return array.map(obj => obj[key]);
+}
+
+let arr1 = [
+        { name: 'Sergey', age: 33 },
+        { name: 'Daria', age: 33 },
+    ];
+
+console.log(getValues(arr1, 'name'));
+console.log(getValues(arr1, 'age'));
