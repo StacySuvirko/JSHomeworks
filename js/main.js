51ed2e0453484a19d172966440e91f7f3ee228f9
Anastasia_Z <fox123_08@mail.ru>
2023-01-13 03:11:33 +0400

    task13 cheched

--- js/main.js
+++ js/main.js
@@ -3,18 +3,31 @@
 // Методы массивов 
 // LearnJS HW
 
-// Task12
+// Task13
 
-function unique(arr) {
-    let arrSorted = [];
-    for (let value of arr) {
-        if (!arrSorted.includes(value)) arrSorted.push(value);
-        }
-        return arrSorted;
-    }
-  
-  let strings = ["кришна", "кришна", "харе", "харе",
-    "харе", "харе", "кришна", "кришна", ":-O"
+let users = [
+    {id: 'john', name: "John Smith", age: 20},
+    {id: 'ann', name: "Ann Smith", age: 24},
+    {id: 'pete', name: "Pete Peterson", age: 31},
   ];
   
-  console.log( unique(strings) ); // кришна, харе, :-O
+  let usersById = groupById(users);
+  
+function groupById(users) {
+    return users.reduce((accum, obj) => {
+        accum[obj.id] = obj;
+        return accum;
+    }, {})
+}
+
+
+console.log(usersById);
+  /*
+  // после вызова у нас должно получиться:
+  
+  usersById = {
+    john: {id: 'john', name: "John Smith", age: 20},
+    ann: {id: 'ann', name: "Ann Smith", age: 24},
+    pete: {id: 'pete', name: "Pete Peterson", age: 31},
+  }
+  */
