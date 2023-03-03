5a0bc1a13299b5c03f4641c3ab9f3bbc1588e3d4
Anastasia_Z <fox123_08@mail.ru>
2023-03-03 17:23:30 +0400

    frilpozhiz burger-menu

--- js/main.js
+++ js/main.js
@@ -1,21 +1,8 @@
-"use strict";
+'use strict';
 
-// const arr1 = [1, 5, 7, 'ddd', 'uyg', 'nnnnn', true, false, 99, null];
-// console.log(arr1);
-// Умножить на 2 все-все элементы массива:
-
-const multidimArr = [1, 2, [3, 4, 5, [6, 7, [8, 9, 10]]]];
-console.log(JSON.stringify(multidimArr));
-
-function doubble() {
- return function(x) {
-     if(!Array.isArray(x)) {
-        return x * 2;
-     } else {
-        return x.map(doubble());
-     }
- }
-}
-
-let d = multidimArr.map(doubble());
-console.log(JSON.stringify(d));
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
