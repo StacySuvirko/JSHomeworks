f652350b7d0fb6bd882131ee5d14a08273fe7bd2
Anastasia_Z <fox123_08@mail.ru>
2023-03-16 19:15:09 +0400

    little img adaptive in media grid element

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
 'use strict';
 
-function sum(...args) {
-    let current = args.reduce((acc, num) => acc + num, 0);
-    function inner(...innerArgs) {
-       for(let i of innerArgs) {
-        current += i;
-       }
-       return inner;
-    }
-    inner.toString = function() {
-        return current;
-    };
-    return inner;
-    
-}
 
-alert(sum(2, 8)(3, 6, 0)(123, 8));
